0e9b4b96133e3c54b5dda472c6f97c8be223bb7a
Jonathan Miller <usmjonathanmiller@jonathans-mbp.dhcp.fnal.gov>
2014-12-09 01:19:18 -0600

    Initial Commit

---
+++ GeoNuSim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GeoNuSim
+//
+//  Created by Jonathan Miller on 12/9/14.
+//  Copyright (c) 2014 Jonathan Miller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
